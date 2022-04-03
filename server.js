ac7bd31ca9123dfb9e9f204ae87bd8dbe6cfbfaa
General Zelgius <mattlow123@gmail.com>
2022-04-03 15:03:11 -0400

    attempt to build a server to test routes

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const db = require('./db/connection');
+const routes = require('./routes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use('/api', routes);
+
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected.');
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+});
